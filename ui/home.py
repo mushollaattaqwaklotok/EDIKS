63a86ece4b156dd8c2ff5d1957f84aaed1be25d7
mushollaattaqwaklotok <mushollaattaqwaklotok@gmail.com>
2025-12-31 21:26:53 +0700

    Create home.py

---
+++ ui/home.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+def show_home():
+    st.markdown("### Selamat Datang di EDIKS 👋")
+
+    st.info(
+        "EDIKS adalah etalase digital warga Dusun Klotok & Simogirang.\n\n"
+        "Aplikasi ini membantu penjual rumahan memasarkan produk secara digital "
+        "dengan pengelolaan amanah oleh Remaja Musholla At-Taqwa."
+    )
+
+    st.markdown("#### 🎯 Tujuan")
+    st.markdown("""
+    - Membantu UMKM rumahan
+    - Meningkatkan ekonomi jamaah
+    - Digitalisasi desa yang sederhana & aman
+    """)
